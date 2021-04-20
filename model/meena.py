1080da4e903066006977cbb859190b3e9fbf77f1
nawnoes <nawnoes@gmail.com>
2021-04-20 15:32:22 +0900

    meena model, config, vocab, pretraining code

--- model/meena.py
+++ model/meena.py
@@ -1,14 +1,8 @@
 from collections import namedtuple
-
-import torch
 from torch import nn
 from model.util import clones, make_std_mask
-import torch.nn.functional as F
-from model.util import log, gumbel_sample, mask_with_tokens, prob_mask_like, get_mask_subset_with_prob
-from model.transformer import PositionalEmbedding,Encoder
-from transformers.activations import get_activation
+from model.transformer import PositionalEmbedding,Encoder, Decoder
 from torch.nn import CrossEntropyLoss
-
 
 MeenaOutput = namedtuple('MeenaOutput', [
   'lm_logits',
@@ -17,24 +11,32 @@ MeenaOutput = namedtuple('MeenaOutput', [
   'decoder_logit',
 ])
 
-
+"""
+Meena best performing model use Evolbed Transformer
+  Model config
+    - 1    encoder block
+    - 13   decoder block
+    - 2560 hidden size
+    - 128  max token length 
+"""
 class Meena(nn.Module):
   def __init__(self,
                vocab_size,
-               dim=512,
+               dim=2560,
                encoder_depth=1,
-               decoder_depth=12,
-               max_seq_len=512,
-               head_num=8,
+               decoder_depth=13,
+               max_seq_len=128,
+               head_num=32,
                dropout=0.1):
     super(Meena, self).__init__()
 
     # Embedding
     self.token_emb = nn.Embedding(vocab_size, dim)
     self.position_emb = PositionalEmbedding(dim, max_seq_len)
+
     # Meena Model
     self.encoders = clones(Encoder(d_model=dim, head_num=head_num, dropout=dropout), encoder_depth)
-    self.decoders = clones(Decoder(d_model=d_model, head_num=head_num, dropout=dropout), decoder_depth)
+    self.decoders = clones(Decoder(d_model=dim, head_num=head_num, dropout=dropout), decoder_depth)
 
     self.norm = nn.LayerNorm(dim)
     self.lm_head = nn.Sequential(
@@ -42,7 +44,7 @@ class Meena(nn.Module):
       nn.Linear(dim, vocab_size)
     )
 
-  def forward(self, input_ids, input_mask):
+  def forward(self, input_ids, input_mask, labels):
     x = self.token_emb(input_ids)
     x = x + self.position_emb(input_ids).type_as(x)
 
@@ -63,6 +65,7 @@ class Meena(nn.Module):
       # Shift so that tokens < n predict n
       shift_logits = lm_logits[..., :-1, :].contiguous()
       shift_labels = labels[..., 1:].contiguous()
+
       # Flatten the tokens
       loss_fct = CrossEntropyLoss(ignore_index=0)
       loss = loss_fct(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
