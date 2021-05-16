26036d88e579acfef807342388a677423a2239a9
nawnoes <nawnoes@gmail.com>
2021-05-16 22:32:44 +0900

    clean code

--- model/transformer.py
+++ model/transformer.py
@@ -1,11 +1,7 @@
 import math
 import torch
 import torch.nn as nn
-from torch.autograd import Variable
 import torch.nn.functional as F
-from torch.nn import CrossEntropyLoss
-from model.util import clones
-from transformers.activations import get_activation
 
 """
 self-Attention의 경우 Query Q, Key K, Value V를 입력으로 받아
@@ -67,7 +63,6 @@ class MultiHeadAttention(nn.Module):
 
   def forward(self, query, key, value, mask = None):
     if mask is not None:
-      # Same mask applied to all h heads.
       mask = mask.unsqueeze(1)
 
     batche_num = query.size(0)
@@ -78,21 +73,11 @@ class MultiHeadAttention(nn.Module):
 
     attention_result, attention_score = self.self_attention(query, key, value, mask, self.causal)
 
-    # 원래의 모양으로 다시 변형해준다.
-    # torch.continuos는 다음행과 열로 이동하기 위한 stride가 변형되어
-    # 메모리 연속적으로 바꿔야 한다!
-    # 참고 문서: https://discuss.pytorch.org/t/contigious-vs-non-contigious-tensor/30107/2
     attention_result = attention_result.transpose(1,2).contiguous().view(batche_num, -1, self.head_num * self.d_k)
 
 
     return self.w_o(attention_result)
 
-"""
-Position-wise Feed-Forward Networks
-FFN(x) = max(0,xW_1 + b_1)W_2+b2
-입력과 출력은 모두 d_model의 dimension을 가지고
-내부의 레이어는 d_model * 4의 dimension을 가진다.
-"""
 class FeedForward(nn.Module):
   def __init__(self,d_model, dropout = 0.1):
     super(FeedForward,self).__init__()
@@ -102,11 +87,7 @@ class FeedForward(nn.Module):
 
   def forward(self, x):
     return self.w_2(self.dropout(F.relu(self.w_1(x))))
-"""
-Layer Normalization
-: layer의 hidden unit들에 대해서 mean과 variance를 구한다. 
-nn.Parameter는 모듈 파라미터로 여겨지는 텐서
-"""
+
 class LayerNorm(nn.Module):
   def __init__(self, features, eps=1e-6):
     super(LayerNorm,self).__init__()
@@ -128,9 +109,6 @@ class ResidualConnection(nn.Module):
   def forward(self, x, sublayer):
     return x + self.dropout((sublayer(self.norm(x))))
 
-"""
-Encoder 블록은 FeedForward 레이어와 MultiHead 어텐션 레이어를 가진다.
-"""
 class Encoder(nn.Module):
   def __init__(self, d_model, head_num, dropout):
     super(Encoder,self).__init__()
@@ -145,10 +123,6 @@ class Encoder(nn.Module):
     x = self.residual_2(x, lambda x: self.feed_forward(x))
     return x
 
-"""
-Decoder 블록은 FeedForward 레이어와 MultiHead 어텐션, Masked Multihead 어텐션 레이어를 가진다.
-MaskedMultiHeadAttention -> MultiHeadAttention(encoder-decoder attention) -> FeedForward
-"""
 class Decoder(nn.Module):
   def __init__(self, d_model,head_num, dropout):
     super(Decoder,self).__init__()
@@ -163,7 +137,6 @@ class Decoder(nn.Module):
 
 
   def forward(self, target, encoder_output, encoder_mask):
-    # target, x, target_mask, input_mask
     x = self.residual_1(target, lambda x: self.masked_multi_head_attention(x, x, x))
     x = self.residual_2(x, lambda x: self.encoder_decoder_attention(x, encoder_output, encoder_output, encoder_mask))
     x = self.residual_3(x, lambda x: self.feed_forward(x))
@@ -176,42 +149,7 @@ class Embeddings(nn.Module):
     self.emb = nn.Embedding(vocab_num,d_model)
     self.d_model = d_model
   def forward(self, x):
-    """
-    1) 임베딩 값에 math.sqrt(self.d_model)을 곱해주는 이유는 무엇인지 찾아볼것
-    2) nn.Embedding에 다시 한번 찾아볼것
-    """
     return self.emb(x) * math.sqrt(self.d_model)
-
-"""
-Positional Encoding
-트랜스포머는 RNN이나 CNN을 사용하지 않기 때문에 입력에 순서 값을 반영해줘야 한다.
-PE (pos,2i) = sin(pos/10000^(2i/d_model))
-PE (pos,2i+1) = cos(pos/10000^(2i/d_model)) 
-"""
-class PositionalEncoding(nn.Module):
-  def __init__(self, max_seq_len, d_model,dropout=0.1):
-    super(PositionalEncoding,self).__init__()
-    self.dropout = nn.Dropout(p=dropout)
-
-    pe = torch.zeros(max_seq_len, d_model)
-
-    position = torch.arange(0,max_seq_len).unsqueeze(1)
-    base = torch.ones(d_model//2).fill_(10000)
-    pow_term = torch.arange(0, d_model, 2) / torch.tensor(d_model,dtype=torch.float32)
-    div_term = torch.pow(base,pow_term)
-
-    pe[:, 0::2] = torch.sin(position / div_term)
-    pe[:, 1::2] = torch.cos(position / div_term)
-
-    pe = pe.unsqueeze(0)
-
-    # pe를 학습되지 않는 변수로 등록
-    self.register_buffer('positional_encoding', pe)
-
-  def forward(self, x):
-    x = x + Variable(self.positional_encoding[:, :x.size(1)], requires_grad=False)
-    return self.dropout(x)
-
 
 class PositionalEmbedding(nn.Module):
   def __init__(self, dim, max_seq_len):
@@ -221,64 +159,6 @@ class PositionalEmbedding(nn.Module):
   def forward(self, x):
     t = torch.arange(x.shape[1], device=x.device)
     return self.embedding(t)
-
-class Generator(nn.Module):
-  def __init__(self, d_model, vocab_num):
-    super(Generator, self).__init__()
-    self.proj_1 = nn.Linear(d_model, d_model*4)
-    self.proj_2 = nn.Linear(d_model*4, vocab_num)
-
-  def forward(self, x):
-    x = self.proj_1(x)
-    x = self.proj_2(x)
-    return x
-class Transformer(nn.Module):
-  def __init__(self,vocab_num, d_model, max_seq_len, head_num, dropout, N):
-    super(Transformer,self).__init__()
-    self.embedding = Embeddings(vocab_num, d_model)
-    self.positional_encoding = PositionalEncoding(max_seq_len,d_model)
-
-    self.encoders = clones(Encoder(d_model=d_model, head_num=head_num, dropout=dropout), N)
-    self.decoders = clones(Decoder(d_model=d_model, head_num=head_num, dropout=dropout), N)
-
-    self.generator = Generator(d_model, vocab_num)
-
-  def forward(self, input, target, input_mask, target_mask, labels=None):
-      x = self.positional_encoding(self.embedding(input))
-      for encoder in self.encoders:
-        x = encoder(x, input_mask)
-
-      target = self.positional_encoding(self.embedding(target))
-      for decoder in self.decoders:
-        # target, encoder_output, target_mask, encoder_mask)
-        target = decoder(target, x, target_mask, input_mask)
-
-      lm_logits = self.generator(target)
-      loss = None
-      if labels is not None:
-        # Shift so that tokens < n predict n
-        shift_logits = lm_logits[..., :-1, :].contiguous()
-        shift_labels = labels[..., 1:].contiguous()
-        # Flatten the tokens
-        loss_fct = CrossEntropyLoss(ignore_index=0)
-        loss = loss_fct(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
-
-      return lm_logits, loss
-  def encode(self,input, input_mask):
-    x = self.positional_encoding(self.embedding(input))
-    for encoder in self.encoders:
-      x = encoder(x, input_mask)
-    return x
-
-  def decode(self, encode_output, encoder_mask, target, target_mask):
-    target = self.positional_encoding(self.embedding(target))
-    for decoder in self.decoders:
-      #target, encoder_output, target_mask, encoder_mask
-      target = decoder(target, encode_output, target_mask, encoder_mask)
-
-    lm_logits = self.generator(target)
-
-    return lm_logits
 
 if __name__=="__main__":
   pass
