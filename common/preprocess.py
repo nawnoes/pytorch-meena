fcc296f0c106ac81bf9adc0784ded53fc13e338e
nawnoes <nawnoes@gmail.com>
2021-09-22 21:12:36 +0900

    preprocess conversation data

--- common/preprocess.py
+++ common/preprocess.py
@@ -23,11 +23,50 @@ def add_turn_info(origin_path, processed_path):
   f.close()
   w_f.close()
 
+def csv2txt_emotional_conversation_data(origin_path, processed_path):
+  file_name = 'emotional_conversation'
+  df = pd.read_csv(f'{origin_path}/{file_name}.csv')
+
+  w_f = open(f'{processed_path}/{file_name}.txt', 'w', encoding='utf-8')
+  for i, row in df.iterrows():
+    # for row in df.itertuples(index=False):
+    for i, item in row.items():
+      if not pd.isna(item):
+        if i == '상황키워드':
+          w_f.write(item + '\n')
+        elif i not in ['번호', '연령', '성별', '신체질환', '감정_소분류', '감정_대분류']:
+          if '사람문장' in i:
+            w_f.write('A: ' + item + '\n')
+          elif '시스템응답' in i:
+            w_f.write('B: ' + item + '\n')
+    w_f.write('\n')
+  w_f.close()
+
 if __name__=='__main__':
   origin_path = '../data/conversation_data/origin'
   processed_path = '../data/conversation_data/processed'
 
-  file_name = 'wellness.txt'
+  file_name = 'emotional_conversation'
+  df = pd.read_csv(f'{origin_path}/{file_name}.csv')
+
+  w_f = open(f'{processed_path}/{file_name}.txt','w',encoding='utf-8')
+  for i, row in df.iterrows():
+  # for row in df.itertuples(index=False):
+    for i, item in row.items():
+      if not pd.isna(item):
+        if i == '상황키워드':
+          w_f.write(item+'\n')
+        elif i not in ['번호', '연령', '성별', '신체질환', '감정_소분류','감정_대분류']:
+          if '사람문장' in i:
+            w_f.write('A: '+item+'\n')
+          elif '시스템응답' in i:
+            w_f.write('B: ' + item + '\n')
+    w_f.write('\n')
+  w_f.close()
+
+
+
+
 
 
 
